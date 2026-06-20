f0c9df32c77c8a77f1bf969f2f89366f4ec465f3
deltea <thcheetah777@gmail.com>
2026-06-20 06:18:14 -0700

    ring hit method

--- scenes/ring/ring.gd
+++ scenes/ring/ring.gd
@@ -10,7 +10,10 @@ func _ready() -> void:
 
 	var tween = create_tween().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT).set_parallel()
 	tween.tween_property(self, "position", target_pos, 1.0)
-	tween.tween_callback(func(): Global.camera.shake(0.1, 0.005)).set_delay(0.4)
-	tween.tween_callback(func(): Global.camera.shake(0.1, 0.005)).set_delay(0.75)
-	tween.tween_callback(func(): Global.camera.shake(0.1, 0.005)).set_delay(0.85)
+	tween.tween_callback(func(): hit()).set_delay(0.4)
+	tween.tween_callback(func(): hit()).set_delay(0.75)
+	tween.tween_callback(func(): hit()).set_delay(0.85)
 
+func hit():
+	Global.camera.shake(0.1, 0.005)
+	# probably play a sound effect here
