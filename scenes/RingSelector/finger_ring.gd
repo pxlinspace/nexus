f3eeb940e8593a7298393d067a90fbdd906e8b63
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-06-20 16:22:33 -0700

    ring selector is now added to game!

--- scenes/RingSelector/finger_ring.gd
+++ scenes/RingSelector/finger_ring.gd
@@ -2,19 +2,19 @@ class_name FingerRing extends Area2D
 
 const RING_HOVER_OFFSET: Vector2 = Vector2(0.0, -10.0)
 
-@export var description: String
-var is_selected: bool = false
+@export var ring_resource: RingResource
+var is_hovered: bool = false
 
 
 func select() -> void:
-	is_selected = true
+	is_hovered = true
 	var tween := create_tween()
 	tween.tween_property(self, "position", RING_HOVER_OFFSET, 0.3) \
 			.set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
 	
 
 func deselect() -> void:
-	is_selected = false
+	is_hovered = false
 	var tween := create_tween()
 	tween.tween_property(self, "position", Vector2.ZERO, 0.3) \
 			.set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
--- scenes/RingSelector/ring_selector.gd
+++ scenes/RingSelector/ring_selector.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+signal ring_selected(ring_resource: RingResource)
+
 const HAND_UP_POSITION: Vector2 = Vector2(147.0, 384.0)
 const HAND_DOWN_POSITION: Vector2 = Vector2(147.0, 688.0)
 
-var selected_ring: FingerRing
+var hovered_ring: FingerRing
 var is_ring_used: bool = false
 @onready var hand: Sprite2D = $Hand
 @onready var mouse_area: Area2D = $MouseArea
@@ -18,37 +20,44 @@ func _ready() -> void:
 			.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	mouse_area.position = get_local_mouse_position()
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("select"):
-		_use_selected_ring()
+		_begin_hovered_ring_animation()
 
 func _on_mouse_area_area_entered(area: Area2D) -> void:
 	if is_ring_used:
 		return
-	selected_ring = area
+	hovered_ring = area
 	area.select()
-	description_label.text = selected_ring.description
+	description_label.text = hovered_ring.ring_resource.desc
 
 
 func _on_mouse_area_area_exited(area: Area2D) -> void:
 	if is_ring_used:
 		return
 	area.deselect()
+	if hovered_ring == area:
+		description_label.text = ""
 
-
-func _use_selected_ring() -> void:
-	if not selected_ring:
+func _begin_hovered_ring_animation() -> void:
+	if not hovered_ring:
 		return
 	is_ring_used = true
-	selected_ring.reparent(self)
+	hovered_ring.reparent(self)
+	var ring_tween = create_tween()
+	ring_tween.tween_property(hovered_ring, "position", hovered_ring.position + Vector2(0, -270.0), 0.4) \
+			.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUART)
+	
 	var hand_tween = create_tween()
 	hand_tween.tween_property(hand, "position", HAND_DOWN_POSITION, 0.75) \
 			.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
-	var ring_tween = create_tween()
-	ring_tween.tween_property(selected_ring, "position", selected_ring.position + Vector2(0, -270.0), 0.4) \
-			.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUART)
+	hand_tween.tween_callback(_use_hovered_ring)
+
+
+func _use_hovered_ring() -> void:
+	ring_selected.emit(hovered_ring.ring_resource)
+	queue_free()
