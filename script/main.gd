726af904843265bdd837247c7ed3161a2af8f707
deltea <thcheetah777@gmail.com>
2026-06-20 02:07:52 -0700

    funi background

---
+++ script/main.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@export var sky_scroll_amount = 3.0
+
+@onready var environment: WorldEnvironment = $WorldEnvironment
+
+@onready var sky_material: ProceduralSkyMaterial = environment.environment.sky.sky_material
+@onready var noise: Noise = sky_material.sky_cover.noise
+
+@onready var turntable: Node3D = $Turntable
+
+func _on_sky_timer_timeout() -> void:
+	turntable.rotation_degrees.y += sky_scroll_amount
