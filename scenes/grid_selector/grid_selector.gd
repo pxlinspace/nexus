566fb61f447059931c3f599a148e5f4f37fd9a34
deltea <thcheetah777@gmail.com>
2026-06-20 23:43:31 -0700

    add placeholder pinching hand thing

--- scenes/grid_selector/grid_selector.gd
+++ scenes/grid_selector/grid_selector.gd
@@ -5,10 +5,11 @@ signal col_selected(col: int)
 @export var grid_col_area_scene: PackedScene
 @onready var mouse_area: Area2D = $MouseArea
 @onready var grid_columns: Node2D = $GridColumns
+@onready var pinch_hand: Sprite2D = $PinchHand
 
 # var hovered_col: GridColArea
 var hovered_col_index: int = -1
-var target_hand_x = 0
+var target_hand_x = 0.0
 
 func _ready() -> void:
 	# spawn mouse column areas
@@ -20,19 +21,26 @@ func _ready() -> void:
 		col.position.y = 360.0 / 2
 		grid_columns.add_child(col)
 
+	var tween = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(pinch_hand, "position:y", 24, 0.5)
 
-func _process(_delta: float) -> void:
+
+func _process(dt: float) -> void:
 	mouse_area.position = get_local_mouse_position()
-
+	pinch_hand.position.x = lerp(pinch_hand.position.x, target_hand_x, 20.0 * dt)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("select") and hovered_col_index >= 0:
 		col_selected.emit(hovered_col_index)
+		var tween = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN)
+		tween.tween_property(pinch_hand, "position:y", -40, 0.5)
+		await tween.finished
 		queue_free()
 
 
 func _on_mouse_area_area_entered(area: Area2D) -> void:
 	hovered_col_index = area.get_index()
+	target_hand_x = area.position.x
 
 
 func _on_mouse_area_area_exited(_area: Area2D) -> void:
