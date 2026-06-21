fc8098b72299f0f14e0e51e2a93a943d30c2db1d
deltea <thcheetah777@gmail.com>
2026-06-21 01:27:49 -0700

    add a main menu scene

---
+++ script/main_menu.gd
@@ -0,0 +1,8 @@
+class_name MainMenu extends Node3D
+
+@export var sky_scroll_amount = 3.0
+
+@onready var turntable: Node3D = $Turntable
+
+func _on_sky_timer_timeout() -> void:
+	turntable.rotation_degrees.y += sky_scroll_amount
