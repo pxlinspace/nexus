a1691a36931f68f36cf590fc6bcd104322c02902
deltea <thcheetah777@gmail.com>
2026-06-21 12:22:12 -0700

    click to play

--- script/main_menu.gd
+++ script/main_menu.gd
@@ -4,6 +4,14 @@ class_name MainMenu extends Node3D
 
 @onready var turntable: Node3D = $Turntable
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("select"):
+		get_tree().change_scene_to_file("res://main.tscn")
+
+func _process(dt: float) -> void:
+	$Turntable/Label3D.position.y = 10.9 + sin(Global.time * 3.0) * 0.1
+	$Turntable/Label3D.rotation_degrees.y = sin(Global.time * 3.0) * 25
+	$Turntable/Label3D.rotation_degrees.x = sin(Global.time * 6.0) * 25
+
 func _on_sky_timer_timeout() -> void:
 	turntable.rotation_degrees.y += sky_scroll_amount
-
