df1d4ce94e17d6ba2b55353e5dbda856155f3621
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-06-20 18:59:54 -0700

    can now select rings on grid

--- scenes/grid_selector/grid_selector.gd
+++ scenes/grid_selector/grid_selector.gd
@@ -3,8 +3,11 @@ class_name GridSelector extends Node2D
 signal col_selected(col: int)
 
 @export var grid_col_area_scene: PackedScene
+@onready var mouse_area: Area2D = $MouseArea
+@onready var grid_columns: Node2D = $GridColumns
 
 # var hovered_col: GridColArea
+var hovered_col_index: int = -1
 var target_hand_x = 0
 
 func _ready() -> void:
@@ -15,4 +18,23 @@ func _ready() -> void:
 		var width = col.get_node("CollisionShape").shape.size.x
 		col.position.x = 640.0 / 2 - (width * num_cols) / 2 + i * width + (width / 2)
 		col.position.y = 360.0 / 2
-		add_child(col)
+		grid_columns.add_child(col)
+
+
+func _process(_delta: float) -> void:
+	mouse_area.position = get_local_mouse_position()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("select") and hovered_col_index >= 0:
+		col_selected.emit(hovered_col_index)
+		queue_free()
+
+
+func _on_mouse_area_area_entered(area: Area2D) -> void:
+	hovered_col_index = area.get_index()
+
+
+func _on_mouse_area_area_exited(_area: Area2D) -> void:
+	if mouse_area.get_overlapping_areas().is_empty():
+		hovered_col_index = -1
--- script/main.gd
+++ script/main.gd
@@ -12,6 +12,7 @@ class_name Main extends Node3D
 
 var camera: Camera
 var grid: Grid
+var selected_ring_resource: RingResource
 
 var round = 0
 var curr_player = 1
@@ -28,14 +29,17 @@ func _on_sky_timer_timeout() -> void:
 	turntable.rotation_degrees.y += sky_scroll_amount
 
 func _on_ring_selector_ring_selected(ring_resource: RingResource) -> void:
+	selected_ring_resource = ring_resource
 	await Global.wait(0.0)
 	Global.camera.change_player(0)
 	# instantiate the column selector
-	# var grid_selector = grid_selector_scene.instantiate()
-	# grid_selector.col_selected = _on_grid_selector_col_selected
+	var grid_selector = grid_selector_scene.instantiate()
+	grid_selector.col_selected.connect(_on_grid_selector_col_selected)
+	hud_canvas.add_child(grid_selector)
 
 func _on_grid_selector_col_selected(col: int) -> void:
 	print(col)
+	grid.drop_ring(curr_player, col, selected_ring_resource)
 
 func _on_grid_ring_dropped() -> void:
 	var ring_selector = ring_selector_scene.instantiate()
