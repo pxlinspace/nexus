629a782bfa8a45b2ef84c92dcacc2f8a13aff882
Neya <neya@tmps-Mac-mini-2.local>
2026-06-20 19:22:56 -0700

    revert

--- script/main.gd
+++ script/main.gd
@@ -10,46 +10,6 @@ extends Node3D
 @onready var turntable: Node3D = $Turntable
 
 var camera: Camera
-<<<<<<< HEAD
 
 func _on_sky_timer_timeout() -> void:
 	turntable.rotation_degrees.y += sky_scroll_amount
-=======
-var grid: Grid
-var selected_ring_resource: RingResource
-
-var round = 0
-var curr_player = 1
-
-func _enter_tree() -> void:
-	Global.main = self
-	grid = $Turntable/Grid
-
-func _ready() -> void:
-	Global.camera.zoom(true)
-	Global.camera.change_player(-1)
-
-func _on_sky_timer_timeout() -> void:
-	turntable.rotation_degrees.y += sky_scroll_amount
-
-func _on_ring_selector_ring_selected(ring_resource: RingResource) -> void:
-	selected_ring_resource = ring_resource
-	await Global.wait(0.0)
-	Global.camera.change_player(0)
-	# instantiate the column selector
-	var grid_selector = grid_selector_scene.instantiate()
-	grid_selector.col_selected.connect(_on_grid_selector_col_selected)
-	hud_canvas.add_child(grid_selector)
-
-func _on_grid_selector_col_selected(col: int) -> void:
-	print(col)
-	grid.drop_ring(curr_player, col, selected_ring_resource)
-
-func _on_grid_ring_dropped() -> void:
-	var ring_selector = ring_selector_scene.instantiate()
-	ring_selector.ring_selected.connect(_on_ring_selector_ring_selected)
-	hud_canvas.add_child(ring_selector)
-	curr_player = 1 if curr_player == 2 else 2
-	print(curr_player)
-	Global.camera.change_player(1 if curr_player == 2 else -1)
->>>>>>> df1d4ce94e17d6ba2b55353e5dbda856155f3621
